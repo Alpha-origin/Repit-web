ea848fc59d3b3c792e69d9402f7a4d3e6e1acd82
han0911 <a01099249559@gmail.com>
2026-04-15 16:47:00 +0900

    fix: 18.2.0버전으로 통함

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -9,7 +9,7 @@ const router = createBrowserRouter([
   {
     path: "*",
     element: <div>페이지를 찾을 수 없어요!</div>, // 404 page
-  },
+  }
 ]);
 
 export default router;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+
 
 function App() {
   return <div>나르샤 프로젝트 첫 페이지</div>;
